e550994b4dcd748104b1f021fdb3db8d065e2b9e
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-06-01 22:29:58 -0300

    Add methods and atributes in new UsedProduct

--- src/entities/UsedProduct.java
+++ src/entities/UsedProduct.java
@@ -1,9 +1,29 @@
 package entities;
 
-public class UsedProduct {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class UsedProduct extends Product {
+
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	Date manufactureDate;
+	
 	public UsedProduct() {
-		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public UsedProduct(String name, Double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+	
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+	
+	@Override
+	public String priceTag() {
+		return super.getName() + " (USED) $" + super.getPrice() + " (Manufacture date: " + dateFormat.format(getManufactureDate()) + ")";
 	}
 
 }
