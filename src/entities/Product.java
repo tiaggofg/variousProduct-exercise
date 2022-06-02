5408b29c35e0cdc47c2d17f4cce72cb55617dd96
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-06-01 22:07:01 -0300

    Add constructors in the Product class

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -5,6 +5,14 @@ public class Product {
 	private String name;
 	private Double price;
 	
+	public Product() {
+	}
+	
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
 	public String getName() {
 		return name;
 	}
