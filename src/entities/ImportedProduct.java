60b78ab094f123e9fee9ed52d82b765b3a8343c7
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-06-02 21:34:26 -0300

    Rename package, create get set and constructors

--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -1,5 +1,23 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product {
 
+	private Double customsFee;
+	
+	public ImportedProduct() {
+		super();
+	}
+	
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+	
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+	
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
 }
